61a028a14dd5508a878d10dec70ecb5472c74afe
Sushil H <sushil@axelerant.com>
2019-10-31 21:10:27 +0530

    Uncommented clonning and composer commands.

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-#read -p "Choose Drupal core branch [8.8.x]: " branch_name
-#branch_name=${branch_name:-8.8.x}
-#echo "$branch_name selected"
-#
-#git clone --branch $branch https://git.drupalcode.org/project/drupal.git docroot
-#lando start
-
-cd docroot
-composer install
+read -p "Choose Drupal core branch [8.8.x]: " branch_name
+branch_name=${branch_name:-8.8.x}
+echo "$branch_name selected"
 
 read -p "Choose profile (standard/demo_umami)[standard]: " profile_name
 profile_name=${profile_name:-standard}
 
+git clone --branch $branch https://git.drupalcode.org/project/drupal.git docroot
+lando start
+
+
 echo "$profile_name selected"
+
+cd docroot
+composer install
+
 
 lando drush site-install $profile_name --account-name=admin --account-pass=admin --db-url=mysql://database:database@database:3306/database --site-name="Drupal 8 Contrib" -y --notify
