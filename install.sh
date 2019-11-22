7aa0d93da10a506dd6e38142f685e0437789fa92
Sushil H <sushil@axelerant.com>
2019-11-22 11:19:14 +0530

    Removed variables to support different environments.

--- install.sh
+++ install.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-read -p "Choose Drupal core branch [8.8.x]: " branch_name
-branch_name=${branch_name:-8.8.x}
-echo "$branch_name selected"
+echo "Cloning 8.9.x branch"
 
-read -p "Choose profile (standard/demo_umami)[standard]: " profile_name
-profile_name=${profile_name:-standard}
-
-git clone --branch $branch https://git.drupalcode.org/project/drupal.git docroot
+git clone --branch 8.8.x https://git.drupalcode.org/project/drupal.git docroot
 lando start
-
-
-echo "$profile_name selected"
 
 cd docroot
 composer install
 
-
-lando drush site-install $profile_name --account-name=admin --account-pass=admin --db-url=mysql://database:database@database:3306/database --site-name="Drupal 8 Contrib" -y --notify
+lando drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://database:database@database:3306/database --site-name="Drupal 8 Contrib" -y --notify
